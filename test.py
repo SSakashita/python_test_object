100902028d55c9673ccfab274f6db397ffa2d577
SSakashita <i.suguru.sakashita@globis.co.jp>
2021-01-11 22:05:30 +0900

    回答の提出
    AVILEN提出課題

--- test.py
+++ test.py
@@ -1,3 +1,37 @@
 """
 このファイルに解答コードを書いてください
 """
+
+FILE_NAME = 'input.txt'
+
+with open(FILE_NAME, mode='rt', encoding='utf-8') as f:
+	d = {}
+	for line in f:
+		# 整数と文字列を辞書に格納
+		line = line.replace('\n', '')
+		if ':' in line:
+			i, s = line.split(':')
+			d[str(i)] = s
+		# 数字のみの場合は入力値として格納
+		else:
+			input_num = int(line)
+
+	# 辞書を昇順に並び替え
+	d = sorted(d.items(), key=lambda x:x[0])
+
+	# 出力の作成
+	answer_text = ''
+	for i, s in d: #もし入力値の約数であれば出力テキストの後ろに追加
+		if input_num % int(i) == 0:
+			answer_text += s
+
+	# 回答の出力
+	if answer_text == '': # もしテキストが空白であれば入力値を返す
+		print(input_num)
+	else: #テキストがあればそれを返す
+		print(answer_text)
+
+
+
+
+
